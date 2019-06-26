6848635567fccc0e1b644cc49818b8519c9378f2
1-cool <38838272+1-cool@users.noreply.github.com>
2019-06-26 13:25:25 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+header("Content-Type: text/html;charset=utf-8");
+$name=$_POST["name"];$num=$_POST["num"];$file=$num.'/'.$num.".txt";
+if(date("Y/m/d")!=date("Y/m/d",filemtime($file)))
+{
+        file_put_contents($file, date("Y/m/d H:i:s")."\n", FILE_APPEND | LOCK_EX);
+        echo "签到成功<br>"; echo $name."<br>";
+        echo "你的学号是："; echo $num."<br>";
+        echo date("Y/m/d H:i:s");
+}
+else
+        echo "签到失败";
+?>
