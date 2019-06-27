0fa952c09c8b7c7d2cdfeeabf8b136378837a5e6
1-cool <38838272+1-cool@users.noreply.github.com>
2019-06-27 10:28:27 +0800

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,11 +1,11 @@
 <?php
 header("Content-Type: text/html;charset=utf-8");
-$name=$_POST["name"];
-$num=$_POST["num"];
-$file=$num.'/'.$num.".txt";
-if(date("Y/m/d")!=date("Y/m/d",filemtime($file)))
+$name=$_POST["name"];                           //获取姓名
+$num=$_POST["num"];                              //获取学号
+$file=$num.'/'.$num.".txt";                       //签到日志位置
+if(date("Y/m/d")!=date("Y/m/d",filemtime($file)))                               //判断今天是否已经签过到
 {
-        file_put_contents($file, date("Y/m/d H:i:s")."\n", FILE_APPEND | LOCK_EX);
+        file_put_contents($file, date("Y/m/d H:i:s")."\n", FILE_APPEND | LOCK_EX);                              //写入签到日志
         echo "签到成功<br>"; echo $name."<br>";
         echo "你的学号是："; echo $num."<br>";
         echo date("Y/m/d H:i:s");
