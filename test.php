66f421a8e3c9b5179d5574498f544360b60a3c9d
1-cool <38838272+1-cool@users.noreply.github.com>
2019-06-27 10:20:37 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,8 @@
 <?php
 header("Content-Type: text/html;charset=utf-8");
-$name=$_POST["name"];$num=$_POST["num"];$file=$num.'/'.$num.".txt";
+$name=$_POST["name"];
+$num=$_POST["num"];
+$file=$num.'/'.$num.".txt";
 if(date("Y/m/d")!=date("Y/m/d",filemtime($file)))
 {
         file_put_contents($file, date("Y/m/d H:i:s")."\n", FILE_APPEND | LOCK_EX);
