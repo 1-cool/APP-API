2771ba34376130cd9fa224930eb51ca7ef29669b
1-cool <38838272+1-cool@users.noreply.github.com>
2019-07-05 18:15:19 +0800

    Update signin.php

--- signin.php
+++ signin.php
@@ -3,13 +3,18 @@ header("Content-Type: text/html;charset=utf-8");
 $name=$_POST["name"];                           //获取姓名
 $num=$_POST["num"];                              //获取学号
 $file=$num.'/'.$num.".txt";                       //签到日志位置
-if(date("Y/m/d")!=date("Y/m/d",filemtime($file)))                               //判断今天是否已经签过到
+if(file_exists($file))                          //判断文件是否存在，来判断是否存在该用户
 {
-        file_put_contents($file, date("Y/m/d H:i:s")."\n", FILE_APPEND | LOCK_EX);                              //写入签到日志
-        echo "签到成功<br>"; echo $name."<br>";
-        echo "你的学号是："; echo $num."<br>";
-        echo date("Y/m/d H:i:s");
+        if(date("Y/m/d")!=date("Y/m/d",filemtime($file)))                               //查看上次修改时间，判断今天是否已经签过到
+        {
+                file_put_contents($file, date("Y/m/d H:i:s")."\n", FILE_APPEND | LOCK_EX);                              //写入签到日志
+                echo "签到成功<br>"; echo $name."<br>";
+                echo "你的学号是："; echo $num."<br>";
+                echo date("Y/m/d H:i:s");
+        }
+        else
+                echo "签到失败";
 }
 else
-        echo "签到失败";
+        echo "不存在此用户";
 ?>
