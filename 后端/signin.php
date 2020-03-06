658562dfad49af38d966dbceb5da59605a9df787
1-cool <38838272+1-cool@users.noreply.github.com>
2020-03-06 22:36:32 +0800

    Update signin.php

--- 后端/signin.php
+++ 后端/signin.php
@@ -1,20 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "user";
-$password = "asdf";
-$dbname = "adf";
 
 header("Content-Type: text/json;charset=utf-8");
-if($_POST["password"]=='wqfeqfsdfgdfhgfdgn')          //判断数据是否为空
+if($_POST["password"]=='asdfdsafasddfdasfdas')          //判断数据是否为空
 {
 	// 创建连接
 	$conn = new mysqli($servername, $username, $password,$dbname);
-	 
+
 	// 检测连接
 	if ($conn->connect_error) {
-	    die("连接失败: " . $conn->connect_error);
+		die("连接失败: " . $conn->connect_error);
 	}
-	
+
 	//插入数据
 	// $data = date("Y/m/d H:i:s");
 	// $sql = "INSERT INTO Info (name, number)
@@ -24,29 +20,30 @@ if($_POST["password"]=='wqfeqfsdfgdfhgfdgn')          //判断数据是否为空
 	// } else {
 	//     echo "Error: " . $sql . "<br>" . $conn->error;
 	// }
-	
-    //输出数据
+
+	//输出数据
 	$sql = "SELECT id, name, number,subject,phone,teachar,qq,sex,count FROM info";
 	$result = $conn->query($sql);
-	if ($result->num_rows > 0) {
-	    // 输出数据
-	    while($row = $result->fetch_assoc()) {
-	    	$data = array
-	                (
-	                        'name'=>$row["name"],
-	                        'num'=>$row["number"],
-	                        'work'=>$row["subject"],
-	                        'phone'=>$row["phone"],
-	                        'teachar'=>$row["teachar"],
-	                        'qq'=>$row["qq"],
-	                        'sex'=>$row["sex"],
-	                        'cont'=>$row["count"]
-	                );
-	                $data = json_encode($data,JSON_UNESCAPED_UNICODE);
-	                echo $data;
-	    }
-	} else {
-	    echo "0 结果";
+	if ($result->num_rows > 0)
+	{
+		// 输出数据
+		while($row = $result->fetch_assoc())
+		{
+			$data = array
+				(
+				 'id'=>$row["id"],
+				 'name'=>$row["name"],
+				 'num'=>$row["number"],
+				 'work'=>$row["subject"],
+				 'phone'=>$row["phone"],
+				 'teachar'=>$row["teachar"],
+				 'qq'=>$row["qq"],
+				 'sex'=>$row["sex"],
+				 'cont'=>$row["count"]
+				);
+			$data = json_encode($data,JSON_UNESCAPED_UNICODE);
+			echo $data;
+		}
 	}
 }
 ?>
