4e6dd7478e8c21a514d458cf1ba11cb91f83edcb
1-cool <38838272+1-cool@users.noreply.github.com>
2020-03-06 20:51:17 +0800

    Update signin.php

--- 后端/signin.php
+++ 后端/signin.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "test123456";
-$password = "test";
-$dbname = "test123456";
+$username = "admin-data";
+$password = "zW4DEKfBWaTYTXsn";
+$dbname = "data";
  
 // 创建连接
 $conn = new mysqli($servername, $username, $password,$dbname);
@@ -10,43 +10,38 @@ $conn = new mysqli($servername, $username, $password,$dbname);
 // 检测连接
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
-} 
-echo "连接成功<br>";
-
-// 使用 sql 创建数据表
-// $sql = "CREATE TABLE Info (
-// id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-// name VARCHAR(30) NOT NULL,
-// number VARCHAR(30) NOT NULL,
-// date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-// )";
- 
-// if ($conn->query($sql) === TRUE) {
-//     echo "Table MyGuests created successfully";
-// } else {
-//     echo "创建数据表错误: " . $conn->error;
-// }
-// echo "<br>";
+}
 
 //插入数据
-$data = date("Y/m/d H:i:s");
-$sql = "INSERT INTO Info (name, number)
-VALUES ('John', '184203')";
-if ($conn->query($sql) === TRUE) {
-    echo "新记录插入成功";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-echo "<br>";
+// $data = date("Y/m/d H:i:s");
+// $sql = "INSERT INTO Info (name, number)
+// VALUES ('John', '184203')";
+// if ($conn->query($sql) === TRUE) {
+//     echo "新记录插入成功";
+// } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+// }
 
 
 //输出数据
-$sql = "SELECT id, name, number,date FROM Info";
+$sql = "SELECT id, name, number,subject,phone,teachar,qq,sex,count FROM info";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // 输出数据
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - 姓名: " . $row["name"]. " - 学号:" . $row["number"] ." - 时间:" . $row["date"] . "<br>";
+    	$data = array
+                (
+                        'name'=>$row["name"],
+                        'num'=>$row["number"],
+                        'work'=>$row["subject"],
+                        'phone'=>$row["phone"],
+                        'teachar'=>$row["teachar"],
+                        'qq'=>$row["qq"],
+                        'sex'=>$row["sex"],
+                        'cont'=>$row["count"]
+                );
+                $data = json_encode($data,JSON_UNESCAPED_UNICODE);
+                echo $data;
     }
 } else {
     echo "0 结果";
