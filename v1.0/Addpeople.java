cf27ce353183a213fc4e5b971425279c6695b27b
1-cool <38838272+1-cool@users.noreply.github.com>
2020-03-13 20:16:07 +0800

    updata

---
+++ v1.0/Addpeople.java
@@ -0,0 +1,432 @@
+package com.example.dianming;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Addpeople {
+   private List<Map<String,Object>> list=new ArrayList<Map<String, Object>>();
+    public void add(){
+
+        Map<String,Object> map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.mingjing);
+        map.put("name","明静");
+        map.put("num","19230103");
+        map.put("work","软件工程");
+        map.put("phone","18168530985");
+        map.put("teacher","王巾雄");
+        map.put("qq","2664931154");
+        map.put("sex","女");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.caoyangyi);
+        map.put("name","曹杨懿");
+        map.put("num","19230301");
+        map.put("work","软件工程");
+        map.put("phone","18651415968");
+        map.put("teacher","王巾雄");
+        map.put("qq","2692034153");
+        map.put("cont",0);
+        map.put("sex","女");
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.shaoxinyi);
+        map.put("name","邵馨怡");
+        map.put("num","19310211");
+        map.put("work","电子科学与技术");
+        map.put("phone","19850516172");
+        map.put("teacher","张晓荣");
+        map.put("qq","321540747");
+        map.put("sex","女");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.zhangzhongxuan);
+        map.put("name","张忠璇");
+        map.put("num","19240305");
+        map.put("work","软件工程嵌入式");
+        map.put("phone","13338975028");
+        map.put("teacher","王巾雄");
+        map.put("qq","3112867070");
+        map.put("sex","女");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.liuanqi);
+        map.put("name","刘安琪");
+        map.put("num","19280108");
+        map.put("work","数字媒体技术");
+        map.put("phone","19850516510");
+        map.put("teacher","冯雪冰");
+        map.put("qq","2990891688");
+        map.put("sex","女");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.zhaoyi);
+        map.put("name","赵怡");
+        map.put("num","19280319");
+        map.put("work","数字媒体技术");
+        map.put("phone","15828322711");
+        map.put("teacher","冯雪冰");
+        map.put("qq","912521733");
+        map.put("sex","女");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.dongyiling);
+        map.put("name","董依琳");
+        map.put("num","19130302");
+        map.put("work","通信工程（嵌入式）");
+        map.put("phone","13615282644");
+        map.put("teacher","李义");
+        map.put("qq","1342338476");
+        map.put("sex","女");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.menlingjun);
+        map.put("name","孟令君");
+        map.put("num","19130104");
+        map.put("work","通信工程（嵌入式）");
+        map.put("phone","18321116117");
+        map.put("teacher","李义");
+        map.put("qq","3135393299");
+        map.put("sex","女");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.weifengqing);
+        map.put("name","魏冯婧");
+        map.put("num","19240304");
+        map.put("work","软件工程（嵌入式）");
+        map.put("phone","15903625336");
+        map.put("teacher","王巾雄");
+        map.put("qq","1419262496");
+        map.put("sex","女");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.liwenjun);
+        map.put("name","李汶俊");
+        map.put("num","19220203");
+        map.put("work","计算机科学与技术（嵌入式）");
+        map.put("phone","18328097958");
+        map.put("teacher","冯雪冰");
+        map.put("qq","454717057");
+        map.put("sex","女");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.wangxishou);
+        map.put("name","汪禧焘");
+        map.put("num","19260329");
+        map.put("work","网络工程（嵌入式）");
+        map.put("phone","13776694494");
+        map.put("teacher","朱亚茹");
+        map.put("qq","1542900212");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.huruiling);
+        map.put("name","胡锐麟");
+        map.put("num","19110212");
+        map.put("work","信息工程");
+        map.put("phone","15396766511");
+        map.put("teacher","杨俊");
+        map.put("qq","1334826870");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.zhangshihao);
+        map.put("name","张世豪");
+        map.put("num","19230232");
+        map.put("work","软件工程");
+        map.put("phone","13839839974");
+        map.put("teacher","王巾雄");
+        map.put("qq","1377152567");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.qianjiakun);
+        map.put("name","钱家琨");
+        map.put("num","19220222");
+        map.put("work","计算机科学与技术（嵌入式培养）");
+        map.put("phone","17372952130");
+        map.put("teacher","冯雪冰");
+        map.put("qq","1945315232");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.zhumaolin);
+        map.put("name","祝茂林");
+        map.put("num","19220233");
+        map.put("work","计算机科学与技术（嵌入式培养）");
+        map.put("phone","13291380651");
+        map.put("teacher","冯雪冰");
+        map.put("qq","1067208331");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.daixu);
+        map.put("name","戴旭");
+        map.put("num","19230212");
+        map.put("work","软件工程");
+        map.put("phone","18168628399");
+        map.put("teacher","王巾雄");
+        map.put("qq","2479338670");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.jiangxiaobo);
+        map.put("name","姜晓波");
+        map.put("num","19270317");
+        map.put("work","物联网工程（嵌入式）");
+        map.put("phone","15883140656");
+        map.put("teacher","张璇");
+        map.put("qq","2311683688");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.qianjiaqi);
+        map.put("name","钱家琪");
+        map.put("num","19240222");
+        map.put("work","软件工程（嵌入式）");
+        map.put("phone","17351792130");
+        map.put("teacher","王巾雄");
+        map.put("qq","2253430255");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.zhouzhanghao);
+        map.put("name","周章浩");
+        map.put("num","19210431");
+        map.put("work","计算机科技与技术");
+        map.put("phone","冯雪冰");
+        map.put("teacher","13999325716");
+        map.put("qq","1833462437");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.rengyikai);
+        map.put("name","任义楷");
+        map.put("num","19130321");
+        map.put("work","通信工程（嵌入式）");
+        map.put("phone","18815931178");
+        map.put("teacher","李义");
+        map.put("qq","524330799");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.songshiqi);
+        map.put("name","宋世麒");
+        map.put("num","19270123");
+        map.put("work","物联网工程（嵌入式）");
+        map.put("phone","13048699517");
+        map.put("teacher","张璇");
+        map.put("qq","2544652949");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.songshiqi);
+        map.put("name","黄晨阳");
+        map.put("num","19130311");
+        map.put("work","通信工程（嵌入式）");
+        map.put("phone","18479445356");
+        map.put("teacher","李义");
+        map.put("qq","1529899279");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.liujunxin);
+        map.put("name","刘俊欣");
+        map.put("num","19230421");
+        map.put("work","软件工程");
+        map.put("phone","15396762838");
+        map.put("teacher","王巾雄");
+        map.put("qq","1142068231");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.liusicheng);
+        map.put("name","刘思程");
+        map.put("num","19210120");
+        map.put("work","计算机科学与技术");
+        map.put("phone","13397003688");
+        map.put("teacher","冯雪冰");
+        map.put("qq","1193237058");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.yangkanglong);
+        map.put("name","杨康龙");
+        map.put("num","19240326");
+        map.put("work","软件工程（嵌入式培养）");
+        map.put("phone","13479710311");
+        map.put("teacher","王巾雄");
+        map.put("qq","1531473459");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.likaishen);
+        map.put("name","李恺晟");
+        map.put("num","19130119");
+        map.put("work","通信工程（嵌入式培养）");
+        map.put("phone","13879182604");
+        map.put("teacher","李义");
+        map.put("qq","932974495");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.zhutian);
+        map.put("name","朱天");
+        map.put("num","19210233");
+        map.put("work","计算机科学与技术");
+        map.put("phone","15862981510");
+        map.put("teacher","冯雪冰");
+        map.put("qq","1436074794");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.xiatian);
+        map.put("name","夏天");
+        map.put("num","19220227");
+        map.put("work","计算机科学与技术");
+        map.put("phone","18914915375");
+        map.put("teacher","冯雪冰");
+        map.put("qq","1457332657");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.lujunreng);
+        map.put("name","卢俊任");
+        map.put("num","19110218");
+        map.put("work","信息工程");
+        map.put("phone","18900353181");
+        map.put("teacher","杨俊");
+        map.put("qq","1050060984");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.taiqichao);
+        map.put("name","邰启超");
+        map.put("num","19230424");
+        map.put("work","软件工程");
+        map.put("phone","15139756126");
+        map.put("teacher","王巾雄");
+        map.put("qq","3187971271");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.qianweiweng);
+        map.put("name","钱未闻");
+        map.put("num","19230422");
+        map.put("work","软件工程");
+        map.put("phone","15195558909");
+        map.put("teacher","王巾雄");
+        map.put("qq","1165812514");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.xugongyou);
+        map.put("name","许恭友");
+        map.put("num","19320125");
+        map.put("work","光电信息科学与工程");
+        map.put("phone","15555444928");
+        map.put("teacher","张晓荣");
+        map.put("qq","11096078760");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.lijieyi);
+        map.put("name","李洁仪");
+        map.put("num","18220303");
+        map.put("work","计算机科学与技术（嵌入式）");
+        map.put("phone","18252759189");
+        map.put("teacher","刘超");
+        map.put("qq","3205633305");
+        map.put("sex","女");
+        map.put("cont",0);
+        list.add(map);
+
+        map=new HashMap<String, Object>();
+        map.put("pic",R.drawable.caoxuanming);
+        map.put("name","曹轩铭");
+        map.put("num","18220109");
+        map.put("work","计算机科学与技术（嵌入式）");
+        map.put("phone","17341944192");
+        map.put("teacher","刘超");
+        map.put("qq","457543493");
+        map.put("sex","男");
+        map.put("cont",0);
+        list.add(map);
+
+
+
+    }
+
+
+    public List<Map<String, Object>> getList() {
+        return list;
+    }
+
+    public void setList(List<Map<String, Object>> list) {
+        this.list = list;
+    }
+}
